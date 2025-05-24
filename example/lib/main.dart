3ff4078ae9cd215d04b6a118121d963e6f0b787b
Samuel Ssekizinvu <external.samuel.ssekizinvu@wuerth.com>
2025-05-24 21:20:55 +0300

    updates

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,28 @@
-import 'package:flutter/material.dart';
+import 'package:dart_macros/dart_macros.dart';
 
-import 'app.dart';
-import 'config/app_config.dart';
+@MacroFile()
+@Define('VERSION', '1.0.0')
+@Define('MAX_ITEMS', 100)
+@Define('PI', 3.14159)
+@Define('DEBUG', true)
+void main() async {
+  await initializeDartMacros();
 
-void main() {
+  print('App Version: ${Macros.get<String>("VERSION")}');
 
-  AppConfig.initialize();
-  // Run the app
+  final items = List.generate(Macros.get<int>('MAX_ITEMS'), (i) => i);
+  print('Generated ${Macros.get("MAX_ITEMS")} $items');
 
+  final area = Macros.get<double>('PI') * 5 * 5;
+  print('Area of circle with radius 5: $area');
+
+  if (Macros.get<bool>('DEBUG')) {
+    print('Debug mode is enabled');
+  }
+
+  // Using predefined macros
+  print('Current file: ${Macros.file}');
+  print('Current line: ${Macros.line}');
+  print('Compilation date: ${Macros.date}');
+  print('Compilation time: ${Macros.time}');
 }
